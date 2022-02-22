edbeda7c110edfbf4be4da7d83f1ea2d2a6fd414
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-02-21 22:16:45 -0300

    feat: adicionado conversao de valores

--- src/br/fai/ex02/Main.java
+++ src/br/fai/ex02/Main.java
@@ -1,5 +1,7 @@
 package br.fai.ex02;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		Main app = new Main();
@@ -7,6 +9,18 @@ public class Main {
 	}
 	
 	private void start() {
+		Scanner scanner = new Scanner(System.in);
+		double valor;
+		int valorInt;
+		
+		System.out.print("Digite o valor do produto: ");
+		
+		valor = scanner.nextDouble();
+		
+		valorInt = (int) valor;
+		
+		System.out.print("\nValor com casas decimais " + valor);
+		System.out.print("| Valor inteiro: " + valorInt);
 		
 	}
 }
